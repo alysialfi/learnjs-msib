13641a98db0107e7367f235d205edcf0f1083935
Poodoop <dickyaristyo43@gmail.com>
2023-09-03 21:22:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const dollar1 = 10
 const dollar2 = 2
 const isMember = true
-const money = null
+let money = null
 
 if (isMember) {
     money = dollar1
 } else {
     money = dollar2
 }
+
+console.log(money)
+//node --watch index.js
